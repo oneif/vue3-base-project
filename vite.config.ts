5b5b0f554b4fb956ac65233dc9ab871f1f2302eb
zhengchuanghui <zhengchuanghui@caseeder.com>
2024-01-05 10:05:04 +0800

    feat：初始化脚手架

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: 'localhost',
+    port: 9000, // 端口号
+    open: true //运行后是否自动在浏览器打开
+  }
+})
